cd6176c77f87cdb1da461d072aaee20d352767c1
kkxmas <kkxmas@gmail.com>
2015-03-02 23:19:53 -0800

    Bare prototype

--- README.md
+++ README.md
@@ -5,23 +5,31 @@
 It's a super rough prototype, yep.
 
 ## Setup:
-1. Make sure you have XCode installed
-1. Install [Node.js](http://nodejs.org)
-2. Install [bower](https://github.com/bower/bower)
+* Make sure you have XCode installed
+* Install [Node.js](http://nodejs.org)
+* Install [bower](https://github.com/bower/bower)
+```
+$ npm install -g bower
   ```
-  $ npm install -g bower
-  ```
- (you may need to use sudo for all the npm installs)
-3. Install Yeoman
+(you may need to use sudo for all the npm installs)
+* Install Yeoman
 ```
 npm install -g yo
 ```
-4. Install Grunt
+* Install Grunt
 ```
 npm install -g grunt-cli
 ```
-5. Run ```npm install``` and ```bower install```
-6. Make sure you have Ruby installed, and run ```gem install compass```
+* Run 
+```
+npm install
+``` 
+and 
+```
+bower install
+```
+* Make sure you have Ruby installed, and run 
+```gem install compass```
 
 ## Development
 You may want to use the Yeoman [AngularJS Generator](https://github.com/yeoman/generator-angular) to add files, etc.
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,49 @@
  */
 angular.module('protoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.messages = [
+      {
+        id: 1,
+        type: 'alert',
+        triggerMessage: "You are 18% behind your quota.",
+        triggerDetailLinkText: "see detail",
+        notifications: [
+          {
+            person: "John Smith",
+            type: "viewed",
+            timestamp: "8:12am"
+          }
+        ],
+        actionList: [
+          {
+            type: 'schedule',
+            message: "Schedule a meeting with John Smith",
+            options: [
+              "9:00 am",
+              "10:00 am",
+              "5:00 pm"
+            ]
+          }
+        ]
+      },
+      {
+        id: 2,
+        type: 'news',
+        triggerMessage: "News bulletin about cool stuff",
+        actionList: [
+          {
+            type: 'slack',
+            message: "Join the Slack channel",
+            url: "http://slack.com"
+          }
+        ]
+      }
     ];
+  })
+  .directive('protoMessage', function() {
+    return {
+      templateUrl: function(elem, attr) {
+        return 'views/protomessage-' + attr.type + '.html';
+      }
+    };
   });
